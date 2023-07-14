1b61bd29b6afe768ee5e4e695362eff5a8c53eaa
Bharadwaj Ravichandran <barry.ravichandran@kitware.com>
2023-07-14 14:16:24 -0400

    Modified setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(name='shared_interest',
       version='0.1',
@@ -7,5 +7,6 @@ setup(name='shared_interest',
       author='Angie Boggust',
       author_email='aboggust@mit.edu',
       license='MIT',
-      packages=['shared_interest'],
+      packages=find_packages('shared_interest/'),
+      package_dir={'': 'shared_interest/'},
       zip_safe=False)
